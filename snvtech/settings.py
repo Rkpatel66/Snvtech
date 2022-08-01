3a566354966fe3fd70887ef779f03e015c093275
Rkpatel66 <mangalarapurajkumar@gmail.com>
2022-08-01 13:27:27 +0530

    first commit

--- snvtech/settings.py
+++ snvtech/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,10 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")  # this is your static folder
 ]
-STATIC_ROOT=os.path(BASE_DIR,'static')
+
+
+STATIC_ROOT=os.path.join(BASE_DIR,'/static/')
+
 
 
 
@@ -130,3 +135,4 @@ STATIC_ROOT=os.path(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
